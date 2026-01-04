a33bcbce796f0c5df6e89b84e543529000f2a39c
Naim <naim.baijid@gmail.com>
2026-01-04 08:21:18 +0600

    update script

--- tmux/.config/tmux/scripts/beb.sh
+++ tmux/.config/tmux/scripts/beb.sh
@@ -5,10 +5,10 @@ if [ -n "$TMUX" ]; then
     echo "Script is running inside an existing tmux session. Killing current session and restarting the script."
     # Get the current tmux session name
     CURRENT_SESSION=$(tmux display-message -p '#S')
-    
+
     # Kill the current session
     tmux kill-session -t $CURRENT_SESSION
-    
+
     # Re-run the script from the beginning (without being inside tmux)
     exec $0
     exit 0
@@ -17,20 +17,23 @@ fi
 # Variables for session names
 SESH_APP='beb-app'
 SESH_BACKEND='beb-server'
+APP_DIR="$HOME/Documents/bebsapati/bebsapati-app"
+BACKEND_DIR="$HOME/Documents/bebsapati/bebsapati-backend/"
 
 # Create the app session if it doesn't exist
 tmux has-session -t $SESH_APP 2>/dev/null
 if [ $? != 0 ]; then
     # app session
-    tmux new-session -d -s $SESH_APP -n 'app'
-    tmux send-keys -t $SESH_APP:app "cd ~/Documents/bebsapati/bebsapati-app/" C-m
+    tmux new-session -d -s $SESH_APP -n 'app' -c "$APP_DIR"
     tmux send-keys -t $SESH_APP:app "nvim" C-m
-    
-    # app terminal
-    tmux new-window -t $SESH_APP -n "appTerm"
-    tmux send-keys -t $SESH_APP:appTerm "cd ~/Documents/bebsapati/bebsapati-app" C-m
-    tmux split-window -h -c "#{pane_current_path}"
 
+    # app terminal
+    tmux new-window -t $SESH_APP -n "appTerm" -c "$APP_DIR"
+    tmux split-window -t "$SESH_APP":appTerm -h -c "$APP_DIR"
+
+
+	# select 1st teminal pane, focus nvim window
+	tmux select-pane -t "$SESH_APP":appTerm.1
     tmux select-window -t $SESH_APP:app
 fi
 
@@ -38,17 +41,19 @@ fi
 tmux has-session -t $SESH_BACKEND 2>/dev/null
 if [ $? != 0 ]; then
     # backend session
-    tmux new-session -d -s $SESH_BACKEND -n "backend"
-    tmux send-keys -t $SESH_BACKEND:backend "cd ~/Documents/bebsapati/bebsapati-backend/" C-m
+    tmux new-session -d -s $SESH_BACKEND -n "backend" -c "$BACKEND_DIR"
     tmux send-keys -t $SESH_BACKEND:backend "nvim" C-m
 
     # backend terminal
-    tmux new-window -t $SESH_BACKEND -n "backendTerm"
-    tmux send-keys -t $SESH_BACKEND:backendTerm "cd ~/Documents/bebsapati/bebsapati-backend" C-m
+    tmux new-window -t $SESH_BACKEND -n "backendTerm" -c "$BACKEND_DIR"
     tmux send-keys -t $SESH_BACKEND:backendTerm "bun dev" C-m
-    tmux split-window -h -c "#{pane_current_path}"
+    tmux split-window -t "$SESH_BACKEND":backendTerm -h -c "$BACKEND_DIR"
+	tmux send-keys -t "$SESH_BACKEND:backendTerm.1" "bun dev" C-m
 
-    tmux select-window -t $SESH_BACKEND:backendTerm
+	# select 2nd pane on terminal, focus nvim window
+	tmux select-pane -t "$SESH_BACKEND":backendTerm.2
+    tmux select-window -t $SESH_BACKEND:backend
+
 fi
 
 # Attach to the app session after setup
